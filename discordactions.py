df3bacf5a666b960b0d4a6a2e1ff3b88554948ae
Alexis LEBEL <lebalexis@gmail.com>
2020-05-05 22:03:50 +0200

    [MISC] Created templates

---
+++ discordactions.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2020 by Alexis LEBEL. Released under MIT License
+#
+import discord.py
+
+class DiscordActions():
+
+    def __init__(self):
+
+        return None
---
+++ probote.py
@@ -0,0 +1,5 @@
+#
+# Copyright (c) 2020 by Alexis LEBEL. Released under MIT License
+#
+import discordactions as da
+import pronoteactions as pa
