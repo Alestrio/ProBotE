a520c4c3120e9eb96470782a6a24f6b41644bae9
Alexis LEBEL <lebalexis@gmail.com>
2020-05-11 20:38:01 +0200

    [Pronote] Connection to Pronote OK

--- pronoteactions.py
+++ pronoteactions.py
@@ -2,42 +2,41 @@
 # Copyright (c) 2020 by Alexis LEBEL. Released under MIT License
 #
 import os
-import requests
 import sys
 import credentials
 import datetime
+import pronotepy
+from pronotepy.ent import ac_reims
 
 class PronoteActions():
 
     def __init__(self):
-        self.isLoggedIn = False
-        r = requests.post('http://127.0.0.1:21727/', json={"type": "login", "username": credentials.username, "password": credentials.password, "url": credentials.url, "cas": credentials.cas})
-        data = r.json()
-        if data['success'] == True:
-            print("Succesfully logged in for Pronote")
-            self.isLoggedIn = True
-        elif data['error'] == "Mauvais identifiants":
-            print('Error : wrong credentials')
+
+        self.client = pronotepy.Client(credentials.url, cookies=ac_reims(credentials.username, credentials.password))
+        if self.client.logged_in:
+            messages = self.client.messages()
+            #for mess in messages: #DEBUG
+            #    print(mess.content)
+        else:
+            print('no login')
         return None
 
     def getHomeworks(self):
-        homeworks = []
-        if self.isLoggedIn:
-            for i in range(len(data['homeworks'])):
-                subject = data['homeworks'][i]['subject']
-                dueDate = datetime.datetime.fromtimestamp(data['homeworks'][i]['until']).strftime('%d-%m-%Y')
-                content = data['homeworks'][i]['content']
-                files = data['homeworks'][i]['files']
-                homeworks.append([dueDate, subject, content, files])
-        return None
+        formattedHomeworks = []
+        if self.client.logged_in:
+            homeworks = self.client.homework(datetime.date.today())
+            for hw in homeworks:
+                formattedHomeworks.append(hw.subject.name + " " + hw.date.strftime('%d - %m - %Y') + " ``` \n" + hw.description + "\n ```")
+        else:
+            self.client = pronotepy.Client(credentials.url, cookies=ac_reims(credentials.username, credentials.password))
+            homeworks = self.client.homework(datetime.date.today())
+            for hw in homeworks:
+                formattedHomeworks.append(hw.subject.name + " " + hw.date.strftime('%d - %m - %Y') + " ``` \n" + hw.description + "\n ```")
+        return formattedHomeworks
 
-    def reconnect(self):
-        self.isLoggedIn = False
-        r = requests.post('http://127.0.0.1:21727/', json={"type": "login", "username": credentials.username, "password": credentials.password, "url": credentials.url, "cas": credentials.cas})
-        data = r.json()
-        if data['success'] == True:
-            print("Succesfully logged in for Pronote")
-            self.isLoggedIn = True
-        elif data['error'] == "Mauvais identifiants":
-            print('Error : wrong credentials')
+    def getLessons(self):
+        formattedLessons = ''
+        return formattedLessons
+
+    def reSync(self):
         return None
