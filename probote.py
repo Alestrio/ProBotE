d0f8737bea238e63cb9a97346e253ad360b4dc72
Alexis LEBEL <lebalexis@gmail.com>
2020-05-18 09:36:29 +0200

    [ProBotE] Corrected start script

--- probote.py
+++ probote.py
@@ -1,8 +1,4 @@
 #
 # Copyright (c) 2020 by Alexis LEBEL. Released under MIT License
 #
-import pronoteactions as pa
 import discordactions as da
-
-pronote = pa.PronoteActions()
-discord = da.DiscordActions()
